877c324c150464ab51bd8b911313dc0d36a9ebad
Jerad Shepherd <jshepherd@brickyardmarketing.com>
2015-03-11 19:29:17 -0400

    Add marker to entered address.

--- maply.js
+++ maply.js
@@ -5,29 +5,49 @@
 
 var url = 'https://maps.googleapis.com/maps/api/geocode/json?address=';
 
-var start_options = {
+// Default map on page load
+var scotts_house = {
   center: { lat: 39.957139, lng: -86.17521599999999 },
   zoom: 18,
   mapTypeId: google.maps.MapTypeId.HYBRID
 };
 
-var map = new google.maps.Map($('#map')[0], start_options);
+// Create the map
+var map = new google.maps.Map($('#map')[0], scotts_house);
 
+// Add a marker
+new google.maps.Marker({
+    position: scotts_house.center,
+    map: map,
+    title:"Eleven Fifty Coding Academy"
+});
+
+// Watch for form submit
 $('form#geocoder').submit(function(ev) {
   ev.preventDefault();
   var address = this.address.value;
-  $.get(url + address).done(function(data) {
+
+  // Ask Google for the address coordinates
+  $.get(url + address).success(function(data) {
     var location = data.results[0].geometry.location;
-    var mapOptions = {
+    var map_options = {
       center: {
         lat: location.lat,
         lng: location.lng
       },
-      zoom: 8
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.HYBRID
     };
 
-    var map = new google.maps.Map($('#map')[0],
-        mapOptions);
+    // Move map to new coordinates
+    map.setOptions(map_options);
+
+    // Add marker
+    var marker = new google.maps.Marker({
+        position: map_options.center,
+        map: map,
+        title: address
+    });
 
   });
 });
